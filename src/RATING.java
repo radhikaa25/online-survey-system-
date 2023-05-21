2363876c7c285a6532c6d6591d8a7b6d76f436d9
Radhika <radhikas2507@gmail.com>
2023-05-21 19:54:37 +0530

    adding files

---
+++ src/RATING.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RATING extends JFrame {
+    private JPanel main;
+    private JTextField txtName;
+    private JTextField txtPhone;
+    private JTextField txtEmail;
+    private JTextArea txtFeedback;
+    private JButton SUBMITButton;
+public RATING() {
+    SUBMITButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = txtName.getText();
+            String phone = txtPhone.getText();
+            String email = txtEmail.getText();
+            String feedback = txtFeedback.getText();
+
+            JOptionPane.showMessageDialog(null, name);
+            JOptionPane.showMessageDialog(null, phone);
+            JOptionPane.showMessageDialog(null, email);
+            JOptionPane.showMessageDialog(null,feedback);
+        }
+    });
+}
+    public static void main(String args[]) {
+        RATING j = new RATING();
+        j.setContentPane(new RATING().main);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setVisible(true);
+        j.pack();
+
+
+    }
+
+}
